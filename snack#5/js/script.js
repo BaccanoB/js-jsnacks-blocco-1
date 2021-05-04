2b75f1be55226be5855de5c72a1fb725b1c94636
BaccanoB <beatrice.baccano@yahoo.it>
2021-05-04 16:00:55 +0200

    terminato es numeri dispari senza funzione

--- snack#5/js/script.js
+++ snack#5/js/script.js
@@ -1,7 +1,19 @@
 // Crea un array vuoto.
 
-// Chiedi per 6 volte all’utente di inserire un numero,
+var array = [];
 
+// Chiedi per 6 volte all’utente di inserire un numero
 // se è dispari (verifichiamolo con una funzione) inseriscilo nell’array.
 
+for (var i = 0; i < 6; i++){
+    var numeroUtente = parseInt(prompt("Inserisci qui un numero"))
+    console.log(numeroUtente);
+
+    if (numeroUtente % 2 !==0) {
+        array.push(numeroUtente);
+    }
+}
+
 // Stampa l'array nella console.
+
+console.log(array);
