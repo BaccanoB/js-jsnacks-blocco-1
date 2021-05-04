3e6b75f25a704c68329a18b8ed48ae745f0c1ed1
BaccanoB <beatrice.baccano@yahoo.it>
2021-05-04 16:14:46 +0200

    inserita funzione nell'esercizio numero dispari

--- snack#5/js/script.js
+++ snack#5/js/script.js
@@ -5,13 +5,30 @@ var array = [];
 // Chiedi per 6 volte all’utente di inserire un numero
 // se è dispari (verifichiamolo con una funzione) inseriscilo nell’array.
 
+// for (var i = 0; i < 6; i++){
+//     var numeroUtente = parseInt(prompt("Inserisci qui un    numero"))
+//     console.log(numeroUtente);
+
+    // if (numeroUtente % 2 !==0) {
+    //     array.push(numeroUtente);
+    // }
+// }
+
+function numeroDispari(num){
+    if (num % 2 !==0) {
+        var dispari = true;
+    }
+    return dispari;
+}
+
 for (var i = 0; i < 6; i++){
     var numeroUtente = parseInt(prompt("Inserisci qui un numero"))
     console.log(numeroUtente);
 
-    if (numeroUtente % 2 !==0) {
+    if (numeroDispari(numeroUtente) == true) {
         array.push(numeroUtente);
     }
+
 }
 
 // Stampa l'array nella console.
